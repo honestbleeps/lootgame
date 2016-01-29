5bfd6cfab25d45dc244d5c2826083e468a029c0c
honestbleeps <steve@openingbands.com>
2016-01-28 19:38:54 -0800

    cleaner light transitions / fix bug where start of cycle has previous/wrong color

--- lyftloot/lyftloot/service.py
+++ lyftloot/lyftloot/service.py
@@ -19,11 +19,12 @@ class LightService:
     on_no_bri = {'on': True, 'bri': 0, 'transitiontime': 0}
     # off = {'on': False, 'transitiontime': 0}
 
-    pink = {'transitiontime' : 0, 'bri': 254, 'xy': [0.4149, 0.1776]}
-    white =  {'transitiontime' : 0, 'bri': 254, 'xy': [0.3227,0.329]}
-    red = {'transitiontime': 1, 'bri': 254, 'xy': [0.7, 0.2986]}
-    red_dark = {'transitiontime': 1, 'bri': 20, 'xy': [0.7, 0.2986]}
-    green = {'transitiontime': 10, 'bri': 254, 'xy': [0.214, 0.709]}
+    pink = {'on': True, 'transitiontime' : 0, 'bri': 254, 'xy': [0.4149, 0.1776]}
+    white =  {'on': True, 'transitiontime' : 0, 'bri': 254, 'xy': [0.3227,0.329]}
+    red = {'on': True, 'transitiontime': 1, 'bri': 254, 'xy': [0.7, 0.2986]}
+    red_dark = {'on': True, 'transitiontime': 1, 'bri': 20, 'xy': [0.7, 0.2986]}
+    green_start = {'on': True, 'transitiontime': 0, 'bri': 1, 'xy': [0.214, 0.709]}
+    green = {'on': True, 'transitiontime': 10, 'bri': 254, 'xy': [0.214, 0.709]}
 
     def __init__(self):
         self.bridge = Bridge('10.59.6.13')
@@ -35,6 +36,8 @@ class LightService:
     @postpone
     def game_start(self):
         c = 0
+        # preset the color before we turn things on
+        self.bridge.set_light(3, self.pink)
         self.bridge.set_light(3, self.on)
         while c < 10:
             self.bridge.set_light(3, self.pink)
@@ -44,10 +47,9 @@ class LightService:
             c += 1
         self.bridge.set_light(3, self.off)
 
-
     @postpone
     def correct(self):
-        self.bridge.set_light(3, self.on_no_bri)
+        self.bridge.set_light(3, self.green_start)
         self.bridge.set_light(3, self.green)
         time.sleep(2)
         self.bridge.set_light(3, self.off)
