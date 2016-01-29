ccf8bdb4e22f2af6d8404c0e20d70e99a312b775
honestbleeps <steve@openingbands.com>
2016-01-28 17:18:11 -0800

    basic light service test / first commit

--- lyftloot/lyftloot/controllers.py
+++ lyftloot/lyftloot/controllers.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from lyftloot import service as light_service
 
 def driver(request):
     context = {}
@@ -12,6 +13,15 @@ def passenger(request):
     context = {}
     return render_to_response("passenger.html", context, RequestContext(request))
 
-
 def home(request):
     return HttpResponseRedirect("/passenger")
+
+def lights_incorrect(request):
+    light_service.incorrect()
+    context = {}
+    return render_to_response("test_lights.html", context, RequestContext(request))
+
+def lights_correct(request):
+    light_service.correct()
+    context = {}
+    return render_to_response("test_lights.html", context, RequestContext(request))
---
+++ lyftloot/lyftloot/service.py
@@ -0,0 +1,63 @@
+from phue import Bridge
+import time
+#import logging
+
+#logging.basicConfig()
+
+b = Bridge('10.59.6.13')
+
+# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+b.connect()
+
+# Get the bridge state (This returns the full dictionary that you can explore)
+#print b.get_api()
+
+on = {'on': True, 'bri': 254, 'transitiontime': 0}
+off = {'on': False, 'bri': 0, 'transitiontime': 0}
+on_no_bri = {'on': True, 'bri': 0, 'transitiontime': 0}
+# off = {'on': False, 'transitiontime': 0}
+
+pink = {'transitiontime' : 0, 'bri': 254, 'xy': [0.4149, 0.1776]}
+white =  {'transitiontime' : 0, 'bri': 254, 'xy': [0.3227,0.329]}
+red = {'transitiontime': 1, 'bri': 254, 'xy': [0.7, 0.2986]}
+red_dark = {'transitiontime': 1, 'bri': 20, 'xy': [0.7, 0.2986]}
+green = {'transitiontime': 10, 'bri': 254, 'xy': [0.214, 0.709]}
+
+@background(0)
+def game_start():
+    c = 0
+    b.set_light(3, on)
+    while c < 10:
+        b.set_light(3, pink)
+        time.sleep(.2)
+        b.set_light(3, white)
+        time.sleep(.2)
+        c += 1
+    b.set_light(3, off)
+
+
+@background(0)
+def correct():
+    b.set_light(3, on_no_bri)
+    b.set_light(3, green)
+    time.sleep(2)
+    b.set_light(3, off)
+
+@background(0)
+def incorrect():
+    c = 0
+    while c < 5:
+        b.set_light(3, red)
+        b.set_light(3, on)
+        time.sleep(.1)
+        b.set_light(3, red_dark)
+        time.sleep(.1)
+        c += 1
+    b.set_light(3, off)
+
+# game_start()
+# correct()
+# incorrect()
+
+
+
